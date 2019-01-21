a799c52187c87d37cb51a92ecbd5ecfb13935f86
Ben Osheroff <ben@gimbo.net>
2019-01-21 07:11:28 -0700

    integrate query fingerprinter

--- lib/shiba/query.rb
+++ lib/shiba/query.rb
@@ -1,12 +1,24 @@
+require 'open3'
+
 module Shiba
   class Query
+
+    PT_FINGERPRINT=File.dirname(__FILE__) + "/pt-fingerprint"
+    @stdin, @stdout = nil
+    def self.get_fingerprint(query)
+      if !@stdin
+        @stdin, @stdout, _, _ = Open3.popen3(PT_FINGERPRINT)
+      end
+      @stdin.puts(query.gsub(/\n/, ' '))
+      @stdout.readline.chomp
+    end
+
     def initialize(sql)
       @sql = sql
     end
 
-    PT_FINGERPRINT=File.dirname(__FILE__) + "/pt-fingerprint"
     def fingerprint
-      `#{PT_FINGERPRINT} --query="#{@sql}"`
+      @fingerprint ||= self.class.get_fingerprint(@sql)
     end
   end
 end
--- lib/shiba/query_watcher.rb
+++ lib/shiba/query_watcher.rb
@@ -1,16 +1,19 @@
+require 'shiba/query'
+
 module Shiba
   # TO use, put this line in config/initializers: Shiba::QueryWatcher.watch
   module QueryWatcher
-    STATEMENTS = {}
+    FINGERPRINTS = {}
     IGNORE = /\.rvm|gem|vendor\/|slow_query_logger|rbenv|test|spec|seed|db/
     ROOT = Rails.root.to_s
-          
+
     def self.watch
       ActiveSupport::Notifications.subscribe('sql.active_record') do |name, start, finish, id, payload|
         line = app_line
-        if !STATEMENTS.key?(line)
-          sql = payload[:sql]
-        
+
+        sql = payload[:sql]
+        query = Shiba::Query.new(sql)
+        if !FINGERPRINTS[query.fingerprint]
           if sql.start_with?("SELECT")
             r = ActiveRecord::Base.connection.execute("EXPLAIN #{sql}")
             index = r.to_a.last[5]
@@ -20,21 +23,21 @@ module Shiba
                 Rails.logger.info("shiba: #{sql} #{explain}")
               end
             end
-          end  
+          end
 
-          STATEMENTS[line] = true
-        end    
+          FINGERPRINTS[query.fingerprint] = true
+        end
       end
     end
-        
+
     def self.app_line
       last_line = caller.detect { |line| line !~ IGNORE }
       if last_line && last_line.start_with?(ROOT)
         last_line = last_line[ROOT.length..-1]
       end
-      
+
       last_line
     end
-        
+
   end
 end
