7662b1dea8b3b97ed7478900cb929985651b5fe3
Ben Osheroff <ben@gimbo.net>
2019-01-22 16:16:40 -0700

    little refactor, little cost knock-down

--- lib/shiba/explain.rb
+++ lib/shiba/explain.rb
@@ -3,7 +3,10 @@ module Shiba
     def initialize(sql, table_sizes)
       @rows = ActiveRecord::Base.connection.select_all("EXPLAIN #{sql}")
       @sizes = table_sizes
+      run_checks!
     end
+
+    attr_reader :cost
 
     def first
       @rows.first.as_json
@@ -17,43 +20,59 @@ module Shiba
       first["Extra"]
     end
 
+    def messages
+      @messages ||= []
+    end
+
     # shiba: {"possible_keys"=>nil, "key"=>nil, "key_len"=>nil, "ref"=>nil, "rows"=>6, "filtered"=>16.67, "Extra"=>"Using where"}
     def to_log
       "possible: '%{possible_keys}', rows: %{rows}, filtered: %{filtered}, cost: #{self.cost},'%{Extra}'" % first.symbolize_keys
     end
 
     def to_h
-      first.merge(cost: cost)
+      first.merge(cost: cost, messages: messages)
     end
 
     def size
       @sizes[first["table"]]
     end
 
-    def cost
-      if first_key
-        return 0
-      elsif first_extra
-        if first_extra =~ /no matching row in const table/
-          return 0
-        elsif first_extra =~ /No tables used/
-          return 0
-        elsif first_extra =~ /Impossible WHERE/
-          return 0
-        elsif first_extra =~ /Select tables optimized away/
-          return 0
-        end
+    IGNORE_PATTERNS = [
+      /no matching row in const table/,
+      /No tables used/,
+      /Impossible WHERE/,
+      /Select tables optimized away/
+    ]
+
+
+    COST_FOR_SIZE = {
+      :small => 0.1,
+      :medium => 0.5,
+      :large => 1.0,
+      nil => 0.9
+    }
+
+    def extra_in_ignore?
+      first_extra && IGNORE_PATTERNS.any? { |p| first_extra =~ p }
+    end
+
+    def check_missing_key
+      return if first_key
+      return if extra_in_ignore?
+
+      addl_cost = COST_FOR_SIZE[size]
+      if first['possible_keys']
+        addl_cost *= 0.5
+        msg = "MySQL dedicded to not use these keys for this query: [" + first["possible_keys"] + "].  "
+        msg += "Depending on data shape, this may or may not be fine."
+        messages << msg
       end
-      case size
-      when :small
-        0.1
-      when :medium
-        0.5
-      when :large
-        1.0
-      when nil
-        0.9
-      end
+      @cost += addl_cost
+    end
+
+    def run_checks!
+      @cost = 0
+      check_missing_key
     end
   end
 end
