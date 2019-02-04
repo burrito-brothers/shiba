5d3f00e90f8afb4e0b42b9eebaeb3ee366780045
Eric <eric@sidecountry.com>
2019-02-04 11:29:17 -0700

    More realitsic fake costs, I think

--- lib/shiba/index.rb
+++ lib/shiba/index.rb
@@ -77,14 +77,18 @@ module Shiba
       tables
     end
 
-    FUZZ_SIZE = 6_001
 
     # Up the cardinality on our indexes.
     # Non uniques have a little less cardinality.
     def self.fuzz!(stats)
-      stats.each do |_,indexes|
+      db = stats.values.first.first['table_schema']
+      table_sizes = self.guess_table_sizes(db)
+
+
+
+      stats.each do |table,indexes|
         indexes.each do |idx|
-          idx['cardinality'] = FUZZ_SIZE
+          idx['cardinality'] = table_sizes[table]
 
           if idx['non_unique'] == 1
             idx['cardinality'] = (idx['cardinality'] * 0.7).round
@@ -112,6 +116,36 @@ module Shiba
       median = table_counts[table_counts.size/2]
 
       return median < MINIMUM_TABLE_SIZE
+    end
+
+    STANDARD_FUZZ_SIZE = 5_000
+
+    # Create fake table sizes based on the table's index count.
+    # The more indexes, the bigger the table. Seems to rank tables fairly well.
+    def self.guess_table_sizes(db)
+      db = Shiba.connection.escape(db)
+      index_count_query = "select TABLE_NAME as table_name, count(*) as index_count
+        from information_schema.statistics where table_schema = '#{db}'
+        and seq_in_index = 1 and index_name not like 'fk_rails%'
+        group by table_name order by index_count"
+
+        index_counts = Shiba.connection.query(index_count_query).to_a
+
+        # 80th table percentile based on number of indexes
+        large_table_idx = (index_counts.size * 0.8).round
+        large_table = index_counts[large_table_idx]
+
+        sizes = Hash[index_counts.map(&:values)]
+
+        sizes.each do |table_name, index_count|
+          if index_count == 0
+            index_count = 1
+          end
+
+          sizes[table_name] = STANDARD_FUZZ_SIZE * (index_count / large_table['index_count'].to_f)
+        end
+
+        sizes
     end
 
     protected
