e3d6481db4d8ff573b50dfb0651926f9f1b15038
Eric <eric@sidecountry.com>
2019-02-05 22:44:10 -0700

    Simple reporting for test runners

--- lib/shiba/railtie.rb
+++ lib/shiba/railtie.rb
@@ -1,34 +1,34 @@
 require 'shiba/query_watcher'
 
 class Shiba::Railtie < Rails::Railtie
+  # Logging is enabled when:
+  #  1. SHIBA_OUT environment variable is set to an existing file path.
+  #  2. RSpec/MiniTest exists, in which case a fallback query log is generated at /tmp
   config.after_initialize do
-    path = `mktemp /tmp/shiba-query.log-#{Time.now.to_i}`.chomp # ENV['SHIBA_OUT']
-    next if !path
+    path = ENV['SHIBA_OUT']
 
-    watcher = log_queries(path)
-    next if !watcher
+    if test_runners_defined?
+      path ||= "/tmp/shiba-query.log-#{Time.now.to_i}"
+    end
 
-    if defined?(RSpec.configure)
-      hook_into_rspec(path)
+    next if path.nil?
+
+    watcher = watch(path)
+    next if watcher.nil?
+
+    at_exit do
+      puts ""
+      system("shiba analyze --file #{path} --test")
     end
   end
 
-  def self.hook_into_rspec(path)
-    RSpec.configure do |c|
-      c.after(:suite) do
-        puts ""
-        system("shiba analyze --file #{path} --test")
-      end
-    end
+  def self.test_runners_defined?
+    defined?(RSpec.configure) || defined?(MiniTest) || defined?(Test::Unit)
   end
 
-  def self.log_queries(path)
-    if File.exist?(path)
-      f = File.open(path, 'a')
-      Shiba::QueryWatcher.watch(f)
-    else
-      $stderr.puts("Shiba could not open '#{path}' for logging.")
-    end
+  def self.watch(path)
+    f = File.open(path, 'a')
+    Shiba::QueryWatcher.watch(f)
   rescue => e
     $stderr.puts("Shiba failed to load")
     $stderr.puts(e.message, e.backtrace.join("\n"))
