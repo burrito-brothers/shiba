92c8fa55fb69603d2926c61221e1122c0d695394
Eric <eric@sidecountry.com>
2019-02-22 15:45:40 -0700

    commit

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -40,7 +40,7 @@ module IntegrationTest
 
       out,_ = run_command({}, "ruby", test_app_path)
 
-      assert File.exist?(File.join(Shiba.path, 'ci.json')), "No log file found at #{Shiba.path}"
+      assert File.exist?(File.join(Shiba.path, 'ci.json')), "No ci log file found at #{Shiba.path}"
     end
 
     it "reviews queries" do
