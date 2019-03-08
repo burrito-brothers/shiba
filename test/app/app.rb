6ca81f5d3c83feeea35c68f908f59fb141b02f4a
Ben Osheroff <ben@gimbo.net>
2019-03-07 19:15:44 -0800

    argh, frenchy.  argh

--- test/app/app.rb
+++ test/app/app.rb
@@ -14,7 +14,7 @@ if ENV['SHIBA_DEBUG']
   ActiveRecord::Base.logger = Logger.new('/dev/stdout')
 end
 
-
+# VERY CAREFUL CHANGING THE LINES OF THIS FILE.  THE TEST WILL BREAK IF YOU ADD LINES
 database_yml = File.join(File.dirname(__FILE__), "..", "database.yml")
 database_yml = database_yml + ".example" unless File.exist?(database_yml)
 
@@ -26,7 +26,6 @@ ActiveRecord::Base.establish_connection(connection[test_env])
 
 org = Organization.create!(name: 'test')
 org.users.create!(email: 'squirrel@example.com')
-
 users = User.where(email: 'squirrel@example.com').to_a # bumpity
 user = users.first
 10.times do |i|
