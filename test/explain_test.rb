6e35584f94421b915b6e11e064d42bf9dd22f6b5
Ben Osheroff <ben@gimbo.net>
2019-03-14 00:00:25 -0700

    split parsers into their own files

--- test/explain_test.rb
+++ test/explain_test.rb
@@ -46,22 +46,6 @@ describe "Explain" do
     it_includes_tag("limited_scan")
   end
 
-  describe "postgres index conditions" do
-    def parse_conditions(conds)
-      Shiba::Parsers::PostgresExplainIndexConditions.new(conds).fields
-    end
-
-    it "parses some index conditions" do
-      assert_equal(%w(rgt), parse_conditions("(rgt > 7)"))
-      assert_equal(%w(type), parse_conditions("((type)::text = 'TimeEntryActivity'::text)"))
-      assert_equal(%w(type), parse_conditions("(((type)::text = ANY ('{Group,GroupBuiltin,GroupAnonymous,GroupNonMember}'::text[])) AND ((type)::text = 'Group'::text))"))
-      assert_equal(%w(type), parse_conditions("((type)::text = 'TimeEntryActivity '' '::text)"))
-      assert_equal(%w(type), parse_conditions("((type)::text = 'TimeEntryActivity '' '::text)"))
-      assert_equal(%w(role_id tracker_id old_status_id), parse_conditions("((role_id = 1) AND (tracker_id = 2) AND (old_status_id = 1))"))
-      assert_equal(["odd column_name"], parse_conditions("(\"odd column_name\" = 123)"))
-    end
-  end
-
   if Shiba.connection.mysql?
     describe "a join" do
       let(:sql) { "SELECT users.* from users INNER JOIN comments on comments.user_id = users.id" }
